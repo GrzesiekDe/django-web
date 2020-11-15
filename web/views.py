cb5d5d4107b5061830eae4d21855da88d1cdedf4
Grzesiek D <danko.grzesiek@gmail.com>
2020-11-15 16:37:26 +0100

    Fix bugs

--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
-from .models import Post
 from django.shortcuts import render
 from django.utils import timezone
+from web.models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
