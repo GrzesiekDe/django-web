b81109ac109fcd22083ca742c36fd9f50121cd1c
Grzesiek D <danko.grzesiek@gmail.com>
2020-11-15 16:38:49 +0100

    DEBUG

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '85_u%*%_ol3p1xm5lgt*24*-lu5gqf-h-)4znu6p+0hn)8mhbn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
