dca34ab0e5e4f49d87eecc033bf5b9d39b2bdfd6
Grzesiek D <danko.grzesiek@gmail.com>
2020-11-15 17:24:57 +0100

    fix bugs

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '85_u%*%_ol3p1xm5lgt*24*-lu5gqf-h-)4znu6p+0hn)8mhbn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'grzesiekde.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
